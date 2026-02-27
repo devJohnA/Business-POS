fada2491b2607ee1c0c5bc2af38a4a1351d78230
John Anthon <delacruzjohnanthon@gmail.com>
2026-02-27 15:10:19 +0800

    asdasd

--- auth-admin/index.php
+++ auth-admin/index.php
@@ -9,26 +9,77 @@ include "navbar.php";
   <div class="container-xxl flex-grow-1 container-p-y">
     <div class="row">
       <div class="col-xxl-4 col-lg-12 col-md-4 order-1">
-        <div class="row">
-          <div class="col-lg-6 col-md-12 col-6 mb-6">
-            <div class="card h-100">
-              <div class="card-body">
-                <div class="card-title d-flex align-items-start justify-content-between mb-4">
-                  <div class="avatar flex-shrink-0">
-                    <img
-                      src="../assets/img/icons/unicons/chart-success.png"
-                      alt="chart success"
-                      class="rounded" />
+                  <div class="row">
+                    <div class="col-lg-6 col-md-12 col-6 mb-6">
+                      <div class="card h-100">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between mb-4">
+                            <div class="avatar flex-shrink-0">
+                              <img
+                                src="../assets/img/icons/unicons/chart-success.png"
+                                alt="chart success"
+                                class="rounded" />
+                            </div>
+                            <div class="dropdown">
+                              <button
+                                class="btn p-0"
+                                type="button"
+                                id="cardOpt3"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <i class="icon-base bx bx-dots-vertical-rounded text-body-secondary"></i>
+                              </button>
+                              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                              </div>
+                            </div>
+                          </div>
+                          <p class="mb-1">Profit</p>
+                          <h4 class="card-title mb-3">$12,628</h4>
+                          <small class="text-success fw-medium"
+                            ><i class="icon-base bx bx-up-arrow-alt"></i> +72.80%</small
+                          >
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 col-6 mb-6">
+                      <div class="card h-100">
+                        <div class="card-body">
+                          <div class="card-title d-flex align-items-start justify-content-between mb-4">
+                            <div class="avatar flex-shrink-0">
+                              <img
+                                src="../assets/img/icons/unicons/wallet-info.png"
+                                alt="wallet info"
+                                class="rounded" />
+                            </div>
+                            <div class="dropdown">
+                              <button
+                                class="btn p-0"
+                                type="button"
+                                id="cardOpt6"
+                                data-bs-toggle="dropdown"
+                                aria-haspopup="true"
+                                aria-expanded="false">
+                                <i class="icon-base bx bx-dots-vertical-rounded text-body-secondary"></i>
+                              </button>
+                              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt6">
+                                <a class="dropdown-item" href="javascript:void(0);">View More</a>
+                                <a class="dropdown-item" href="javascript:void(0);">Delete</a>
+                              </div>
+                            </div>
+                          </div>
+                          <p class="mb-1">Sales</p>
+                          <h4 class="card-title mb-3">$4,679</h4>
+                          <small class="text-success fw-medium"
+                            ><i class="icon-base bx bx-up-arrow-alt"></i> +28.42%</small
+                          >
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <p class="mb-1">Profit</p>
-                <h4 class="card-title mb-3">$12,628</h4>
-                <small class="text-success fw-medium"><i class="icon-base bx bx-up-arrow-alt"></i> +72.80%</small>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
       
       <!-- Total Revenue -->
       <div class="col-12 col-xxl-8 order-2 order-md-3 order-xxl-2 mb-6 total-revenue">
